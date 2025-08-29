bc1e1a938684e308afcc34753a064cd2c716ec82
Anondoraydev <Anondo554@gmail.com>
2025-08-30 00:30:19 +0600

    auth contollar addend

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AuthRoutes } from "../modules/auth/auth.route";
 import { ParcelRoutes } from "../modules/parcel/parcel.route";
 import { UserRoutes } from "../modules/user/user.route";
 
@@ -9,10 +10,10 @@ const moduleRoutes = [
     path: "/user",
     route: UserRoutes,
   },
-  // {
-  //   path: "/auth",
-  //   route: AuthRoutes,
-  // },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
   {
     path: "/parcel",
     route: ParcelRoutes,
