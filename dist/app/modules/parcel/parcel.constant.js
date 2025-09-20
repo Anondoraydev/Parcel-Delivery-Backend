4630dc8c8948d396dd6af62cad9f7a5803b922d8
Anondo Ray <Anondo554@gmail.com>
2025-09-20 10:24:05 +0600

    final commit

--- dist/app/modules/parcel/parcel.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parcelSearchableFields = void 0;
-exports.parcelSearchableFields = [
-    "currentStatus",
-    "receiver.name",
-    "receiver.email",
-    "packageDetails.type",
-];
